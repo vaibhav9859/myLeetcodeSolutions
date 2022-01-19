a3040e0d1e32c8085e35cdd36c6acffabbbd28af
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-01-19 15:11:46 +0530

    Added solution - LeetHub

--- Minimum Platforms - GFG/minimum-platforms.cpp
+++ Minimum Platforms - GFG/minimum-platforms.cpp
@@ -10,46 +10,27 @@ class Solution{
     public:
     //Function to find the minimum number of platforms required at the
     //railway station such that no train waits.
-    // int findPlatform(int arr[], int dep[], int n)
-    // {
-    // 	sort(arr, arr+n);
-    // 	sort(dep, dep+n);
-    // 	int mp = 1, np = 0;
-    	
-    // 	for(int i=1, j =0; i<n and j<n; ){
-    // 	    if(arr[i] < dep[j]){
-    // 	        i++; mp++;
-    // 	        if(np<mp) np = mp;
-    // 	    }
-    // 	    else if(arr[i]>dep[j]){
-    // 	        j++; mp--;
-    // 	        if(np<mp) np = mp;
-    // 	    }
-    // 	    else {i++; j++;}
-    // 	}
-    	
-    // 	return np;
-    // }
     int findPlatform(int arr[], int dep[], int n)
     {
-    	// Your code here
-    	sort(arr,arr+n);
-    	sort(dep,dep+n);
-    	int maxPlatform = 1;
-    	int curPlats = 1;
-    	int i=1;int j=0;
-    	while(i<n){
-    	    if(arr[i] > dep[j]){
-    	        curPlats--;
-    	        j++;
-    	    }else{
-    	        curPlats++;
-    	        i++;
+    	sort(arr, arr+n);
+    	sort(dep, dep+n);
+    	int mp = 1, np = 1;
+    	
+    	for(int i=1, j =0; i<n and j<n; ){
+    	    if(arr[i] <= dep[j]){
+    	        i++; mp++;
+    	        if(np<mp) np = mp;
     	    }
-    	    maxPlatform = max(maxPlatform,curPlats);
+    	    else if(arr[i]>dep[j]){
+    	        j++; mp--;
+    	        if(np<mp) np = mp;
+    	    }
+    	    
     	}
-    	return maxPlatform;
+    	
+    	return np;
     }
+
 };
 
 
