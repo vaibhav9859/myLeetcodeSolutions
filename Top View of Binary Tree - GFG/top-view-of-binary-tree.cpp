2400c43c1bb35ff948b9b5d3a1db08d351212f41
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-02-24 20:11:02 +0530

    Added solution - LeetHub

---
+++ Top View of Binary Tree - GFG/top-view-of-binary-tree.cpp
@@ -0,0 +1,169 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+ // } Driver Code Ends
+
+
+/*
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+*/
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    
+    void tvHelper(Node* root, map<int, pair<int, int>> &m, int &maxLeft, int &maxRight, int curPos, int lev){
+        if(!root) return;
+        
+        // if(maxLeft >= curPos){
+        //     maxLeft = curPos; 
+            if(!m[curPos].second) m[curPos] = {root->data, lev};
+            else if (m[curPos].second > lev) m[curPos] = {root->data, lev};
+            
+        // }
+        // if(maxRight <= curPos){
+        //     maxRight = curPos; 
+            if(!m[curPos].second) m[curPos] = {root->data, lev};
+            else if(m[curPos].second > lev) m[curPos] = {root->data, lev};
+        // }
+        
+        tvHelper(root->left, m, maxLeft, maxRight, curPos-1, lev+1);
+        tvHelper(root->right, m, maxLeft, maxRight, curPos+1, lev+1);
+    }
+    
+    vector<int> topView(Node *root)
+    {
+        //Your code here
+        
+        vector<int> output; int maxLeft = 0, maxRight = 0; 
+        if(!root) return output;
+        map<int, pair<int, int>> m; 
+        m[0] = {root->data, 1};
+        tvHelper(root, m, maxLeft, maxRight, 0, 1);
+        
+        // for(auto pr: m){
+        //     cout<<pr.first<<" "<<pr.second.first<<" "<<pr.second.second<<endl;
+        // }
+        // cout<<endl;
+        for(auto pr: m){
+            output.push_back(pr.second.first);
+        }
+        
+        return output;
+    }
+
+};
+
+
+
+// { Driver Code Starts.
+
+int main() {
+    int tc;
+    cin>>tc;
+    cin.ignore(256, '\n');
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Solution ob;
+        Node *root = buildTree(treeString);
+        vector<int> vec = ob.topView(root);
+        for(int x : vec)
+            cout<<x<<" ";
+        cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
