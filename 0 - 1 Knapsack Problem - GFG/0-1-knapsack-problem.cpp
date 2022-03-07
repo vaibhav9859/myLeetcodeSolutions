b48316306875c2ac32e52e7281d28e664caa41f2
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-03-07 18:32:51 +0530

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -0,0 +1,88 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       int** dp = new int*[n+1];
+       for(int i=0; i<=n; i++){
+           dp[i] = new int[W+1];
+           for(int j=0; j<=W; j++){
+               dp[i][j] = 0;
+           }
+       }
+       
+    //   memset(dp, 0, sizeof(dp));
+       
+    //   for(int i=0; i<n; i++){
+    //       for(int j=0; j<W; j++){
+    //           cout<<dp[i][j]<<" ";
+    //       }
+    //       cout<<endl;
+    //   }
+       
+      for(int i=1; i<=n; i++){
+          for(int j=1; j<=W; j++){
+              if(wt[i-1] <= j) dp[i][j] = max(dp[i-1][j], val[i-1] + dp[i-1][j - wt[i-1]]);
+              else dp[i][j] = dp[i-1][j];
+          }
+      }
+       
+       return dp[n][W];
+    }
+};
+
+
+
+// n, w, val[], w[]
+
+// N * W
+
+
+// 0 + 3, 0
+
+//   0     1   2   3   4
+// 0 0     0   0   0   0
+// 1 0     0   0   0   1
+// 2 0     0   0   0   1  
+// 3 0     3   3 
+
+
+
+// { Driver Code Starts.
+
+int main()
+ {
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //reading number of elements and weight
+        int n, w;
+        cin>>n>>w;
+        
+        int val[n];
+        int wt[n];
+        
+        //inserting the values
+        for(int i=0;i<n;i++)
+            cin>>val[i];
+        
+        //inserting the weights
+        for(int i=0;i<n;i++)
+            cin>>wt[i];
+        Solution ob;
+        //calling method knapSack()
+        cout<<ob.knapSack(w, wt, val, n)<<endl;
+        
+    }
+	return 0;
+}  // } Driver Code Ends
