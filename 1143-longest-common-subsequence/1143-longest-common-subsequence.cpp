b1c54435b2b6cdc444c0d999df01653479e86a62
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-03-24 19:17:01 +0530

    Time: 24 ms (61.27%), Space: 10.7 MB (61.06%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    int dp[1003][1003];
-    int longestCommonSubsequence(string &text1, string &text2, int i=0, int j=0,int flag = 1) {
-        if(flag) {flag = 0; memset(dp, -1, sizeof(dp));}
-        
-        if(i == text1.size() or j == text2.size()) return 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        if(text1[i] == text2[j]){
-            return dp[i][j] = 1+longestCommonSubsequence(text1, text2, i+1, j+1, 0);
+    int arr[1002][1002];
+    
+    int longestCommonSubsequence(string &text1, string &text2, int i=0, int j=0) {
+        if(i==text1.size() or j==text2.size()){
+            return 0;
         }
-        else{
-            return dp[i][j] = max(longestCommonSubsequence(text1, text2, i+1, j, 0),
-                                    longestCommonSubsequence(text1, text2, i, j+1, 0));
+        if(i==0 and j==0){
+            memset(arr, -1, sizeof(arr));
         }
+        
+        if(arr[i][j] != -1) return arr[i][j];
+        
+
+        if(text1[i] == text2[j]){ 
+            
+            return arr[i][j] =  1 + longestCommonSubsequence(text1, text2, i+1, j+1);
+        }
+        
+        return arr[i][j] = max(longestCommonSubsequence(text1, text2, i+1, j), longestCommonSubsequence(text1, text2, i, j+1) );
+        
+
+        
     }
 };
