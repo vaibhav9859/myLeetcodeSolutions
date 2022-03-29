a7b7bb30dba809afb563416e05c7f703a40a6e24
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-03-29 16:20:57 +0530

    Added solution - LeetHub

--- Print all possible strings - GFG/print-all-possible-strings.cpp
+++ Print all possible strings - GFG/print-all-possible-strings.cpp
@@ -25,27 +25,26 @@ int main()
 
 /*You have to complete this function*/
 
-void helper(char str[], int sz, int i, string curStr, vector<string> &output
-, set<string> &s){
+void helper(char str[], int sz, int i, string curStr, vector<string> &output){
     if(i==sz){
         
-        if(!curStr.empty() and curStr[curStr.size()-1] == ' ') curStr.pop_back();
-        if(s.find(curStr) != s.end()) return;
+        // if(!curStr.empty() and curStr[curStr.size()-1] == ' ') curStr.pop_back();
+        // if(s.find(curStr) != s.end()) return;
         output.push_back(curStr); 
-        s.insert(curStr);
+        // s.insert(curStr);
         return;
     }
     
-    helper(str, sz, i+1, curStr + str[i], output, s);
-    helper(str, sz, i+1, curStr + str[i]+" ", output, s);
+    helper(str, sz, i+1, curStr + str[i], output);
+    if(i!=sz-1) helper(str, sz, i+1, curStr + str[i]+" ", output);
 }
 
 vector<string>  spaceString(char str[])
 {
 //Your code here
-    set<string> s;
+    // set<string> s;
     vector<string> output;
-    helper(str, strlen(str), 0, "", output, s);
+    helper(str, strlen(str), 0, "", output);
     
     return output;
 }
