f6528c84161fc46608181a25d142509fe6ed4737
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-03-30 15:33:52 +0530

    Time: 0 ms (100.00%), Space: 7 MB (30.92%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -9,24 +9,25 @@ public:
             for(int i=cs; i<=ce; i++){
                 output.push_back(matrix[rs][i]);
             }
-            rs++;
+            if(++rs > re) break;
+            
 
             for(int i=rs; i<=re; i++){
                 output.push_back(matrix[i][ce]);
             }
-            ce--;
+            if(--ce < cs) break;
             
-            if(rs>re or cs>ce) break;
+          
             for(int i=ce; i>=cs; i--){
                 output.push_back(matrix[re][i]);
             }
-            re--;
+            if(--re < rs) break;
             
-            if(rs>re or cs>ce) break;
+            
             for(int i=re; i>=rs; i--){
                 output.push_back(matrix[i][cs]);
             }
-            cs++;
+            if(++cs > ce) break;
         }
         
         return output;
