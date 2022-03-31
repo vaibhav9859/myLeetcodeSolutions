73c111620212b063f04a88993372aff2d4d630b7
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-03-31 16:45:18 +0530

    Time: 125 ms (39.83%), Space: 69.4 MB (54.53%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -4,21 +4,15 @@ public:
         
         int prevBal = 0, curBal = 0;
         int sz = gas.size(); int start = 0;
-        int ans = 0;
         
         for(int i=0; i<sz; i++){
             
-            int diff = gas[i] - cost[i];
-            if(diff >= 0){
-                curBal += diff;
-            }
-            else if(diff + curBal >= 0){
-                curBal  = diff + curBal;
-            }
-            else{
+            curBal += gas[i] - cost[i];
+            if(curBal < 0){
+                prevBal += curBal; curBal = 0;
                 start = i+1;
-                prevBal += diff + curBal; curBal = 0;
             }
+
         }
         
         if(start<sz and curBal + prevBal >= 0) return start;
