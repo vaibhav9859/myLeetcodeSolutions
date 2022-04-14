ded30ca8d61f5e42af2b3e158e7370bc9a219187
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-04-14 14:15:30 +0530

    Time: 128 ms (7.34%), Space: 43.4 MB (6.01%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp[101][101][201];
+    
+    bool isInterleave(string s1, string s2, string s3) {
+        memset(dp, -1, sizeof(dp));
+        return helper(s1, s2, s3, 0, 0, 0);
+    }
+    
+    
+    bool helper(string s1, string s2, string s3, int fptr, int sptr, int tptr){
+        
+        if(fptr == s1.size() and sptr == s2.size() and tptr == s3.size()) return true;
+        
+        if(tptr == s3.size()) return false;
+        
+        if(dp[fptr][sptr][tptr] != -1) return dp[fptr][sptr][tptr];
+        
+        bool opt1 = false, opt2 = false;
+        
+        if(fptr < s1.size() and s3[tptr] == s1[fptr]) opt1 = helper(s1, s2, s3, fptr+1, sptr, tptr+1);
+        if(sptr < s2.size() and s3[tptr] == s2[sptr]) opt2 = helper(s1, s2, s3, fptr, sptr+1, tptr+1);
+        
+        return dp[fptr][sptr][tptr] = opt1 or opt2;
+    }
+};
