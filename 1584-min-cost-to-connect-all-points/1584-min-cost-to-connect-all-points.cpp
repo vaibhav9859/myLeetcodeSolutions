037f8086d1c2f20c589d541d47cab879d35c6f7b
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-04-26 22:23:37 +0530

    Time: 331 ms (69.76%), Space: 58.2 MB (65.64%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,22 +1,40 @@
 class Solution {
 public:
+    
+    //For video explanation visit this link
+    //https://www.youtube.com/watch?v=K7LqWeA9WSg
     int minCostConnectPoints(vector<vector<int>>& points) {
+        
+        //Creating min heap of distance, startNode, endNode
+        //priority queues in STL are always sorted in terms of first value when use pairs or pairs of pairs
         priority_queue< pair<int, pair<int, int> >, vector<pair<int, pair<int, int> >>, greater<pair<int, pair<int, int>> > > min_heap;
+        
+        //Using set to know what all nodes I have already considered and taken in my path
+        //I shouldn't take those nodes again
         unordered_set<int> uset; int totalPoints = points.size();
         
+        //Initially the cost will be zero
         int ansCost = 0;
         
+        //Now in the beginning I am putting 0th node, and the distance from itself will be zero
+        //Below {0,{0,0}} is showing zero distance from node zero to node zero
         min_heap.push({0, {0, 0}});
         
+        //Now I have to keep doing some tasks till all the nodes have been visited
+        //I am making sure that by keeping the check on size of my set
         while(!min_heap.empty() and uset.size() != totalPoints){
             int curCost = min_heap.top().first; int curStart = min_heap.top().second.first, curEnd = min_heap.top().second.second;
             min_heap.pop();  
             
+            //If my curNode is already present in the set I shouldn't consider that move to next minimum distance node
             if(uset.find(curEnd) != uset.end()) continue;
             
-            // cout<<curCost<<" "<<curStart<<" "<<curEnd<<endl;
+            //Adding the cost to curCost and inserting the node in set coz now this node has been considered in the path
             ansCost += curCost; uset.insert(curEnd);
             
+            
+            //Now calculating distance to all the points except the points which are already visited
+            //After calculation putting them in heap
             for(int i=0; i<totalPoints; i++){
                 if(uset.find(i) != uset.end()) continue;
                 
