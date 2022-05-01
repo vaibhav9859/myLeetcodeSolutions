512d7831bc142c3a10018c959dedb8bb9802fb72
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-05-01 15:12:33 +0530

    Added solution - LeetHub

--- Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
@@ -5,31 +5,48 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        // Code here 
+
+        vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         int rows = image.size(), cols = image[0].size();
         
-        vector<vector<int>> vis(rows, vector<int>(cols, 0));
+        queue<pair<int, int>> q; vector<vector<int>> vis(rows, vector<int>(cols, 0));
         
-        ffHelper(image, sr, sc, newColor, image[sr][sc], rows, cols, vis);
-        
+        q.push({sr, sc}); int desColor = image[sr][sc]; 
+                
+        while(!q.empty()){
+
+            int sz = q.size();
+            
+            for(int i=0; i<sz; i++){
+                int curR = q.front().first, curC = q.front().second; q.pop();
+                
+                // cout<<curR<<" "<<curC<<endl;
+                if(vis[curR][curC]){
+                    continue;
+                }
+                vis[curR][curC] = 1; 
+                image[curR][curC] = newColor;
+                
+                int temp[4][4] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+                
+                for(int i=0; i<4; i++){
+                    int newCurR = curR + temp[i][0];
+                    int newCurC = curC + temp[i][1];
+                    
+                    if(newCurR>=0 and newCurR<rows and newCurC>=0 and newCurC<cols and image[newCurR][newCurC] == desColor 
+                       and !vis[newCurR][newCurC]){
+                        q.push({newCurR, newCurC});
+                    }
+                }
+                
+                
+            }
+           
+        }
         return image;
+        
     }
     
-    
-    void ffHelper(vector<vector<int>>& image, int sr, int sc, int newColour, int desColour, int rows, int cols, 
-    vector<vector<int>> &vis){
-        
-        if(sr < 0 or sc < 0 or sr >= rows or sc >= cols or vis[sr][sc] or image[sr][sc] != desColour) return;
-        
-        vis[sr][sc] = 1;
-        image[sr][sc] = newColour;
-        
-        ffHelper(image, sr, sc+1, newColour, desColour, rows, cols, vis);
-        ffHelper(image, sr+1, sc, newColour, desColour, rows, cols, vis);
-        ffHelper(image, sr, sc-1, newColour, desColour, rows, cols, vis);
-        ffHelper(image, sr-1, sc, newColour, desColour, rows, cols, vis);
-    }
 };
 
 // { Driver Code Starts.
