a3a98a1c8eae574eb9138e3a706d434205b49bc7
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-05-05 10:51:42 +0530

    Time: 5 ms (13.56%), Space: 6.7 MB (88.02%) - LeetHub

---
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -0,0 +1,86 @@
+class MyStack {
+public:
+    queue<int> q1, q2;
+    
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        if(!q1.empty()) q1.push(x);
+        else if(!q2.empty()) q2.push(x);
+        else q1.push(x);
+    }
+    
+    int pop() {
+        int ans;
+        if(!q1.empty()){
+            while(q1.size() != 1){
+                int val = q1.front(); q1.pop();
+                q2.push(val);
+            }
+            
+            ans = q1.front();
+            
+            // q2.push(ans);
+            q1.pop();
+            return ans;
+        }
+        else if(!q2.empty()){
+            while(q2.size() != 1){
+                int val = q2.front(); q2.pop();
+                q1.push(val);
+            }
+            
+            ans = q2.front();
+            q2.pop();
+            // q1.push(ans);
+            
+            return ans;
+        }   
+        
+        return ans;
+    }
+    
+    int top() {
+        int ans;
+        if(!q1.empty()){
+            while(q1.size() != 1){
+                int val = q1.front(); q1.pop();
+                q2.push(val);
+            }
+            
+            ans = q1.front(); q1.pop();
+            
+            q2.push(ans);
+            
+            return ans;
+        }
+        else if(!q2.empty()){
+            while(q2.size() != 1){
+                int val = q2.front(); q2.pop();
+                q1.push(val);
+            }
+            
+            ans = q2.front(); q2.pop();
+            q1.push(ans);
+            
+            return ans;
+        }
+        return ans;
+    }
+    
+    bool empty() {
+        if(q1.empty() and q2.empty()) return true;
+        return false;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
