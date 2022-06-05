964145db2837de5e15012ed8da41d1dbcebb3c8c
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-06-05 16:12:45 +0530

    Time: 584 ms (5.13%), Space: 70.9 MB (43.35%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.cpp
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
-        vector<int> adj[n+1]; vector<int> colour(n+1, -1);
+        vector<int> adj[n+1];
         
-        for(int i=0; i<dislikes.size(); i++){
-            adj[dislikes[i][0]].push_back(dislikes[i][1]);
-            adj[dislikes[i][1]].push_back(dislikes[i][0]);
+        for(auto pr: dislikes){
+            adj[pr[0]].push_back(pr[1]);
+            adj[pr[1]].push_back(pr[0]);
         }
         
-        // for(int i=1; i<=n; i++){
-        //     cout<<i<<"-->";
-        //     for(auto a: adj[i]) cout<<a<<" ";
-        //     cout<<endl;
-        // }
+        vector<int> color(n+1, -1);
         
         for(int i=1; i<=n; i++){
-            if(colour[i] == -1){
-                if(bfs(adj, i, colour) == false) return false;
+            if(color[i] == -1 and  !biparPossible(adj, i, color)){
+                return false;
             }
         }
-
-        
+       
         return true;
     }
     
-    bool bfs(vector<int> adj[], int startNode, vector<int> &colour){
-        queue<int> q; q.push(startNode); colour[startNode] = 1; int assCol = 1;
+    bool biparPossible(vector<int> adj[], int src, vector<int> &color){
+        
+        color[src] = 0;
+        int col = 0;
+        queue<int> q; 
+        q.push(src);
         
         while(!q.empty()){
-            int sz = q.size(); int curCol = 1 - assCol; assCol = curCol;
-            for(int i=0; i<sz; i++){
-                int cur = q.front(); q.pop(); 
-                for(auto &node: adj[cur]){
-                    if(colour[node] == -1){
-                        colour[node] = curCol;
-                        q.push(node);
-                    }
-                    else{
-                        if(colour[node] != curCol) return false;
-                    }
+            
+            int curNode = q.front();
+            q.pop();
+            col = color[curNode];
+            
+            for(auto &adjNode: adj[curNode]){
+                if(color[adjNode] == -1){
+                    color[adjNode] = 1 - col;
+                    q.push(adjNode);
                 }
+                else if(color[adjNode] == col) return false;
             }
         }
+        
         return true;
     }
 };
