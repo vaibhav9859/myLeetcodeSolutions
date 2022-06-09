e83c4b6d5da66d2127b6702822ad7991b045383d
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-06-09 19:36:58 +0530

    Time: 173 ms (60.88%), Space: 50.7 MB (57.13%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int nEdges = connections.size();
+        int extraEdges = nEdges - (n-1);
+        
+        if(extraEdges < 0) return -1;
+        
+        vector<int> adj[n];
+        for(int i=0; i<nEdges; i++){
+            int u = connections[i][0];
+            int v = connections[i][1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        int connComp = numConComp(adj, n);
+        
+        int reqEdges = connComp - 1;
+        
+        if(reqEdges <= extraEdges){
+            return reqEdges;
+        }
+        
+        return reqEdges;
+    }
+    
+    int numConComp(vector<int> adj[], int n){
+        vector<int> vis(n, 0);
+        int connComp = 0;
+        
+        for(int i=0; i<n; i++){
+            if(!vis[i]){
+                dfs(adj, vis, i);
+                connComp++;
+            }
+        }
+        
+        return connComp;
+    }
+    
+    void dfs(vector<int> adj[], vector<int> &vis, int node){
+        vis[node] = 1;
+        
+        for(auto &adjNode: adj[node]){
+            if(!vis[adjNode]){
+                dfs(adj, vis, adjNode);
+            }
+        }
+    }
+};
