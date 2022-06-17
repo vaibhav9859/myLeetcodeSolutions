01c83b47bb40040866bb3b16b69611cf6644ff1c
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-06-17 12:23:30 +0530

    Time: 19 ms (43.21%), Space: 21.1 MB (98.60%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -18,27 +18,39 @@ public:
         if(root and !root->left and !root->right) return 1;
         int val = cameraNeed(root, count);
         
+        //Below addition is done coz
+        //When current node's both left and right child were covered
+        //the current node told to its parent that we need a camera
+        //this addition here will be needed 
+        //when root nodes return -1, which means
+        //root node says to its parent that it requires a camera
         if(val == -1) count++;
         return count;
     }
     
     
     int cameraNeed(TreeNode* root, int &count){
-        // cout<<count<<" ";
+
         if(!root) return 1;
         if(!root->left and !root->right) return -1;
         
         int leftNeed = cameraNeed(root->left, count);
         int rightNeed = cameraNeed(root->right, count);
         
+        //When any of the child node says it needs camera
         if(leftNeed == -1 or rightNeed == -1){
             count++;
             return 0;
             
-        }else if(leftNeed == 1 and rightNeed == 1){
+        }
+        // When both children say they are covered
+        // so present node tells its parent that we need camera
+        //but present node does not directly but the camera at its position
+        else if(leftNeed == 1 and rightNeed == 1){
             return -1;
         }
         
+        //Otherwise we know that we need to return present node is covered
         return 1;
     }
 };
