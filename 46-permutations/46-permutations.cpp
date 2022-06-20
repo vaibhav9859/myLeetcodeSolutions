6c3111a1db931b04bc91a24bba391359556b9755
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-06-20 16:44:57 +0530

    Time: 8 ms (28.91%), Space: 7.6 MB (91.90%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    
-    //Time Complexity: Generating n! permutation, for every permutation looping from 0 to n-1
+    //This one is the swapping solution
+    //Time Complexity: Generating n! permutation, for every permutation looping from position to n-1
     //Time Complexity: O(n! * n)
-    //Space Complexity: O(n)-For map + O(n) for current vector/arrangement
-    //There is one more solution where you don't need to use extra map, That is direclty by swapping 
-    //swapping solution is also submitted
+    //Space Complexity: Not using any extra space, using directly the given array "nums" for our purpose
+    
     vector<vector<int>> permute(vector<int>& nums) {
         int sz = nums.size();
         vector<vector<int>> output;
