6526d3e7ee41480c8c2900ddedffc9cfac292ac1
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-06-20 15:47:12 +0530

    Time: 7 ms (37.72%), Space: 7.7 MB (55.65%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -4,6 +4,8 @@ public:
     //Time Complexity: Generating n! permutation, for every permutation looping from 0 to n-1
     //Time Complexity: O(n! * n)
     //Space Complexity: O(n)-For map + O(n) for current vector/arrangement
+    //There is one more solution where you don't need to use extra map, That is direclty by swapping 
+    //swapping solution is also submitted
     vector<vector<int>> permute(vector<int>& nums) {
         int sz = nums.size();
         unordered_map<int, int> presentMap;
