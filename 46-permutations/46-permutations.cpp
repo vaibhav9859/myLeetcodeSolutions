29e4dbb51c9371bd5025fb183bdb733e54f83249
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-06-20 16:43:11 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (55.65%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -8,33 +8,24 @@ public:
     //swapping solution is also submitted
     vector<vector<int>> permute(vector<int>& nums) {
         int sz = nums.size();
-        unordered_map<int, int> presentMap;
-        vector<int> curVec;
         vector<vector<int>> output;
         
-        permuteFinder(nums, sz, 0, presentMap, curVec, output);
+        permuteFinder(nums, sz, 0, output);
          
         return output;
     }
     
     
-    void permuteFinder(vector<int> &nums, int &sz, int pos, unordered_map<int, int> &presentMap, vector<int> &curVec, vector<vector<int>> &output){
-        if(sz == pos){
-            output.push_back(curVec);
+    void permuteFinder(vector<int> &nums, int &sz, int pos, vector<vector<int>> &output){
+        if(pos == sz-1){
+            output.push_back(nums);
             return;
         }
         
-        for(int index=0; index<sz; index++){
-            if(!presentMap[index]){
-                
-                presentMap[index] = 1;
-                curVec.push_back(nums[index]);
-                
-                permuteFinder(nums, sz, pos+1, presentMap, curVec, output);
-                
-                presentMap[index] = 0;
-                curVec.pop_back();
-            }
+        for(int index=pos; index<sz; index++){
+            swap(nums[pos], nums[index]);
+            permuteFinder(nums, sz, pos+1, output);
+            swap(nums[pos], nums[index]);
         }
  
     }
