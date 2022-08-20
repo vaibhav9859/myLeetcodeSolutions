97646852fc8b59ea652990b6d906587f2cd7c613
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-08-20 22:44:50 +0530

    Time: 8 ms (39.57%), Space: 11.3 MB (29.48%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
-        int low = 0, high = arr.size()-1, mid;
+//     int search(vector<int>& nums, int target) {
         
+//     }
+    
+    int search(vector<int> arr, int tar){
+
+        int low = 0, high = arr.size()-1;
+
+
         while(low <= high){
-            cout<<low<<" "<<high<<endl;
-            mid = (low + high)/2;
-            
-            if(target == arr[mid]) return mid;
-            else if(arr[low] <= arr[mid]){
-                if(target>=arr[low] and target<=arr[mid]) high = mid-1;
-                else low = mid+1;
-            }
-            else {
-                if(target >= arr[mid] and target <= arr[high]) low = mid+1;
-                else high = mid - 1;                
-            }
+                int mid = (low + high)/2;
+
+                if(arr[mid] == tar) return mid;
+                else{
+                    if(mid-1 >= low and arr[low] <= arr[mid-1]){
+                        if(tar >= arr[low] and tar <= arr[mid-1]) high = mid-1;
+                        else low = mid+1;
+                    }
+                    else{
+                        if(mid+1 <= high and tar >= arr[mid+1] and tar <= arr[high]) low = mid+1;
+                        else high = mid-1;
+                    }
+                }
         }
-        
+
         return -1;
     }
 };
