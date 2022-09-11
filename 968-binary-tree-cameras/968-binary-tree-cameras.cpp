62343e627907355f681048eab867fc8b84869235
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2022-09-12 01:00:49 +0530

    Time: 16 ms (64.73%), Space: 21.3 MB (54.49%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -12,45 +12,32 @@
 class Solution {
 public:
     int minCameraCover(TreeNode* root) {
-        int count = 0;
-        
         if(!root) return 0;
-        if(root and !root->left and !root->right) return 1;
-        int val = cameraNeed(root, count);
+        int count = 0;
+        int val = camNeed(root, count);
+        if(val==-1) return ++count;
         
-        //Below addition is done coz
-        //When current node's both left and right child were covered
-        //the current node told to its parent that we need a camera
-        //this addition here will be needed 
-        //when root nodes return -1, which means
-        //root node says to its parent that it requires a camera
-        if(val == -1) count++;
         return count;
     }
     
     
-    int cameraNeed(TreeNode* root, int &count){
-
+    int camNeed(TreeNode* root, int &count){
         if(!root) return 1;
         if(!root->left and !root->right) return -1;
         
-        int leftNeed = cameraNeed(root->left, count);
-        int rightNeed = cameraNeed(root->right, count);
+        int l = camNeed(root->left, count);
+        int r = camNeed(root->right, count);
         
-        //When any of the child node says it needs camera
-        if(leftNeed == -1 or rightNeed == -1){
+        
+        if(l == -1 or r==-1){
             count++;
             return 0;
-            
         }
-        // When both children say they are covered
-        // so present node tells its parent that we need camera
-        //but present node does not directly but the camera at its position
-        else if(leftNeed == 1 and rightNeed == 1){
+        
+        else if(l==1 and r==1){
             return -1;
         }
         
-        //Otherwise we know that we need to return present node is covered
         return 1;
     }
 };
