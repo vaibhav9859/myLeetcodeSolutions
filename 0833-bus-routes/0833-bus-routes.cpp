2798b47f39dacf304e9e2afc2540698516122664
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-01-03 19:18:16 +0530

    Time: 464 ms (43.72%), Space: 59.7 MB (71.59%) - LeetHub

---
+++ 0833-bus-routes/0833-bus-routes.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        if(source == target) return 0;
+        
+        // stop to route mapping .. route indices starting from 0
+        unordered_map<int, vector<int>> stopToRoute;
+
+        int numRoutes = routes.size();
+
+        for(int i=0; i<numRoutes; i++){
+            for(auto &curStp : routes[i]){
+                stopToRoute[curStp].push_back(i);
+            }
+        }
+
+        queue<int> qRouteIdx;
+        unordered_set<int> visRouteIdx;
+        for(auto &routeIdx : stopToRoute[source]){
+            qRouteIdx.push(routeIdx);
+            visRouteIdx.insert(routeIdx);
+        }
+
+        int busReq = 1;
+
+        while(!qRouteIdx.empty()){
+            int qSz = qRouteIdx.size();
+            while(qSz--){
+                int curRouteIdx = qRouteIdx.front();
+                qRouteIdx.pop();
+                
+                for(auto &stop : routes[curRouteIdx]) {
+                    if(stop == target) return busReq;
+                    else{
+                        for(auto &routeIdx : stopToRoute[stop]){
+                            if(visRouteIdx.find(routeIdx) == visRouteIdx.end()){
+                                visRouteIdx.insert(routeIdx);
+                                qRouteIdx.push(routeIdx);
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Let's go to a new bus - new Route
+            busReq++;
+        }
+
+        return -1;
+    }
+};
