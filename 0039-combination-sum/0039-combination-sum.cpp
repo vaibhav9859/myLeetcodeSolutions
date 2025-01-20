82a837029390875589392db3dd7adb1a60d06d5d
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-01-20 10:51:37 +0530

    Time: 91 ms (9.04%), Space: 72.4 MB (6.38%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> allComb;
+        vector<int> curComb;
+
+        allCombi(candidates, target, curComb, 0, 0, allComb);
+        return allComb;
+    }
+
+    void allCombi(vector<int> &arr, int &target, vector<int> curComb, int curSum, int curIndex, vector<vector<int>> &allComb){
+        if(curIndex >= arr.size() or curSum > target) return;
+        if(curSum == target){
+            allComb.push_back(curComb);
+            return;
+        }
+
+        // consider cur value in comb and don't move forward
+        curComb.push_back(arr[curIndex]);
+        allCombi(arr, target, curComb, curSum+arr[curIndex], curIndex, allComb);
+        
+        // consider cur value in comb and move ahead
+        // allCombi(arr, target, curComb, curSum+arr[curIndex], curIndex+1, allComb);
+
+
+        curComb.pop_back();
+        // don't consider the cur value and move ahead
+        allCombi(arr, target, curComb, curSum, curIndex+1, allComb);
+
+    }
+};
