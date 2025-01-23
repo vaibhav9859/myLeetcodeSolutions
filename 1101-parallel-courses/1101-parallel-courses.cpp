240e3b2f09e4cf25bf0cfc6c50a158ec0baf6478
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-01-24 01:10:18 +0530

    Time: 4 ms (84.79%), Space: 31.1 MB (64.84%) - LeetHub

---
+++ 1101-parallel-courses/1101-parallel-courses.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int minimumSemesters(int n, vector<vector<int>>& relations) {
+        vector<int> adjList[n+1];
+        unordered_map<int, int> inDeg;
+
+        for(auto &edge: relations){
+            adjList[edge[0]].push_back(edge[1]);
+            inDeg[edge[1]]++;
+        }
+
+        queue<int> bfsQueue;
+        int semsReq = 0;
+        int visCount = 0;
+
+        for(int i=1; i<=n; i++){
+            if(inDeg.find(i) == inDeg.end()){
+                bfsQueue.push(i);
+                // visCount++;
+            }
+        }
+
+        
+        
+        while(!bfsQueue.empty()){
+            int sz = bfsQueue.size();
+            semsReq++;
+            // visCount++;
+
+            while(sz--){
+                int curCourse = bfsQueue.front();
+                bfsQueue.pop();
+                visCount++;
+
+                for(auto &newCourse: adjList[curCourse]){
+
+                    if(--inDeg[newCourse] == 0){
+                        bfsQueue.push(newCourse);
+                    } 
+
+                    // if(inDeg[newCourse] < 0) return -1;
+                }
+            }
+        }
+
+        int ans = visCount == n ? semsReq : -1; 
+
+        return ans;
+        // return semsReq;
+    }
+};
