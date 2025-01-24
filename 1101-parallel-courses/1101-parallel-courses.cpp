3229a7b070f9c2b60cc6ee66e5abe166d5087194
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-01-24 11:44:20 +0530

    Time: 8 ms (64.09%), Space: 31.1 MB (64.84%) - LeetHub

--- 1101-parallel-courses/1101-parallel-courses.cpp
+++ 1101-parallel-courses/1101-parallel-courses.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
     int minimumSemesters(int n, vector<vector<int>>& relations) {
-        vector<int> adjList[n+1];
-        unordered_map<int, int> inDeg;
+        
+        // adjList
+        vector<int> graph[n+1];
 
-        for(auto &edgeNodes: relations){
-            adjList[edgeNodes[0]].push_back(edgeNodes[1]);
-            inDeg[edgeNodes[1]]++;
+        // inDeg Cal
+        unordered_map<int, int> courseInDeg;
+
+        for(auto &relation: relations){
+            graph[relation[0]].push_back(relation[1]);
+            courseInDeg[relation[1]]++;
         }
 
-        queue<int> bfsQueue;
         int semsReq = 0;
-        int visCount = 0;
+        int studied = 0;
+
+        queue<int> bfsQ;
 
         for(int i=1; i<=n; i++){
-            if(inDeg.find(i) == inDeg.end()){
-                bfsQueue.push(i);
+            if(courseInDeg.find(i) == courseInDeg.end()){
+                bfsQ.push(i);
             }
         }
- 
-        while(!bfsQueue.empty()){
-            int sz = bfsQueue.size();
+
+        while(!bfsQ.empty()){
+            int sz = bfsQ.size();
             semsReq++;
-
+            
             while(sz--){
-                int curCourse = bfsQueue.front();
-                bfsQueue.pop();
-                visCount++;
+                int curCourse = bfsQ.front();
+                bfsQ.pop();
 
-                for(auto &newCourse: adjList[curCourse]){
+                studied++;
 
-                    if(--inDeg[newCourse] == 0){
-                        bfsQueue.push(newCourse);
+                for(auto &adjCourse: graph[curCourse]){
+                    if(--courseInDeg[adjCourse] == 0){
+                        bfsQ.push(adjCourse);
                     } 
-
-                    // if(inDeg[newCourse] < 0) return -1;
                 }
             }
         }
-
-        int ans = visCount == n ? semsReq : -1; 
-
-
-        return ans;
-        // return semsReq;
+       
+        return studied == n ? semsReq : -1;
     }
-};
+};  
