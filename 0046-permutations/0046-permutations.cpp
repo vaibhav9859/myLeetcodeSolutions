c0ddddde693459c251b7992a189daa7cb1202a13
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-02-12 22:46:50 +0530

    Time: 0 ms (100%), Space: 10.5 MB (70.85%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -7,7 +7,7 @@ public:
         return allPer;
     }
 
-    void allPermute(vector<int> nums, int &sz, int curIdx, vector<vector<int>> &allPer){
+    void allPermute(vector<int> &nums, int &sz, int curIdx, vector<vector<int>> &allPer){
         if(curIdx >= sz){
             allPer.push_back(nums);
             return;
@@ -16,6 +16,7 @@ public:
         for(int itr=curIdx; itr<sz; itr++){
             swap(nums[curIdx], nums[itr]);
             allPermute(nums, sz, curIdx+1, allPer);
+            swap(nums[curIdx], nums[itr]);
         }
     }
 };
