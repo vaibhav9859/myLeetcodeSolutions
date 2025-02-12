277e6c8c912e233a35fe2c0488d7105616437ea1
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-02-12 22:45:56 +0530

    Time: 3 ms (29.88%), Space: 11.8 MB (17.74%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> allPer;
+        int sz = nums.size();
+        allPermute(nums, sz, 0, allPer);
+        return allPer;
+    }
+
+    void allPermute(vector<int> nums, int &sz, int curIdx, vector<vector<int>> &allPer){
+        if(curIdx >= sz){
+            allPer.push_back(nums);
+            return;
+        }
+
+        for(int itr=curIdx; itr<sz; itr++){
+            swap(nums[curIdx], nums[itr]);
+            allPermute(nums, sz, curIdx+1, allPer);
+        }
+    }
+};
