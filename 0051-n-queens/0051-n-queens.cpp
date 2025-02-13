0f723a3f37b84134e1dbd8fb64f9d8b711c2d412
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-02-13 14:39:04 +0530

    Time: 3 ms (65.24%), Space: 10.3 MB (46.08%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -4,64 +4,38 @@ public:
         string str(n, '.');
         vector<string> board(n, str);
         vector<vector<string>> allComb;
-        solve(board, 0, n, allComb);
+
+        vector<int> checkRow(n, 0);
+        vector<int> checkLowDiag(2*n, 0);
+        vector<int> checkUpDiag(2*n, 0);
+
+        solve(board, 0, n, allComb, checkRow, checkLowDiag, checkUpDiag);
         return allComb;
     }
 
-    void solve(vector<string> &board, int curCol, int &sz, vector<vector<string>> &allComb){
+    void solve(vector<string> &board, int curCol, int &sz, vector<vector<string>> &allComb, vector<int> &checkRow, vector<int> &checkLowDiag, vector<int> &checkUpDiag){
         if(curCol == sz){
             allComb.push_back(board);
             return;
         }
 
         for(int row = 0; row < sz; row++){
-            if(isSafe(row, curCol, board, sz)){
+            if(isSafe(row, curCol, board, sz, checkRow, checkLowDiag, checkUpDiag)){
                 board[row][curCol] = 'Q';
-                solve(board, curCol+1, sz, allComb);
+                checkRow[row] = 1;
+                checkLowDiag[row + curCol] = 1;
+                checkUpDiag[(sz-1)+(curCol-row)] = 1;
+                solve(board, curCol+1, sz, allComb, checkRow, checkLowDiag, checkUpDiag);
+                checkRow[row] = 0;
+                checkLowDiag[row + curCol] = 0;
+                checkUpDiag[(sz-1)+(curCol-row)] = 0;
                 board[row][curCol] = '.';
             }
         }
     }
 
-    bool isSafe(int curRow, int curCol, vector<string> &board, int &sz){
-        int dupRow = curRow;
-        int dupCol = curCol;
-        // Check curRow
-        // while(curCol < sz){
-        //     if(board[curRow][curCol++] == 'Q') return false;
-        // }
-
-        curCol = dupCol;
-        while(curCol >= 0){
-            if(board[curRow][curCol--] == 'Q') return false;
-        }
-
-        // Check right diagonal
-        // curCol = dupCol;
-        // while(curRow <sz and curCol < sz){
-        //     if(board[curRow++][curCol++] == 'Q') return false;
-        // }
-
-        curRow = dupRow;
-        curCol = dupCol;
-        while(curRow >= 0 and curCol >= 0){
-            if(board[curRow--][curCol--] == 'Q') return false;
-        }
-        
-        
-        // check left diagonal
-        // curRow = dupRow;
-        // curCol = dupCol;
-        // while(curRow >= 0 and curCol < sz){
-        //     if(board[curRow--][curCol++] == 'Q') return false;
-        // }
-
-        curRow = dupRow;
-        curCol = dupCol;
-        while(curRow < sz and curCol >= 0){
-            if(board[curRow++][curCol--] == 'Q') return false;
-        }
-
+    bool isSafe(int curRow, int curCol, vector<string> &board, int &sz,vector<int> &checkRow, vector<int> &checkLowDiag, vector<int> &checkUpDiag){
+        if(checkRow[curRow] or checkLowDiag[curRow + curCol] or checkUpDiag[(sz-1)+(curCol - curRow)]) return false;
         return true;
     }
 };
