50fb1632bab3f97f4c6f1d578821a116a2b4ee0f
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-02-15 17:58:45 +0530

    Time: 3 ms (59.4%), Space: 16.7 MB (36.32%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    struct compare {
+        bool operator()(const vector<int>& a, const vector<int>& b) {
+            return a[1] > b[1]; // Min heap based on end times
+        }
+    };
+
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        int sz = intervals.size();
+        priority_queue<vector<int>, vector<vector<int>>, compare> minHeap; 
+
+        int minRooms = 1;
+
+        for(int i=0; i < sz; i++){
+            if(minHeap.empty())
+            {
+                minHeap.push(intervals[i]);
+            } 
+            else if(!minHeap.empty())
+            {
+                int endTime = minHeap.top()[1];
+                if(endTime <= intervals[i][0])
+                {
+                    minHeap.pop();
+                    minHeap.push(intervals[i]);
+                } else 
+                {
+                    minHeap.push(intervals[i]);
+                    int sz1 = minHeap.size();
+                    minRooms = max(minRooms, sz1);
+                }
+            }
+        }
+
+        return minRooms;
+    }
+};
