b6480dbbfccd8ad5cbeb2f413473d3b74542c6f3
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-02-24 14:10:59 +0530

    Time: 657 ms (5.08%), Space: 296.5 MB (12.53%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -13,26 +13,34 @@ public:
 
         if(prevIdx != -1 and dp[curIdx][prevIdx] != -1) return dp[curIdx][prevIdx];
 
-        if(prevIdx == -1){
+        // if(prevIdx == -1){
+        //     // consider present element in seq
+        //     len = max(len, 1 + maxLen(nums, curIdx+1, curIdx, dp));
+
+        //     // don't consider present element in seq
+        //     len = max(len, maxLen(nums, curIdx+1, prevIdx, dp));
+        // }
+        // else{
+        //     if(nums[curIdx] > nums[prevIdx]){
+        //         // consider present element in seq
+        //         len = max(len, 1 + maxLen(nums, curIdx+1, curIdx, dp));
+
+        //         // don't consider present element in seq
+        //         len = max(len, maxLen(nums, curIdx+1, prevIdx, dp));
+        //     }
+        //     else {
+        //         // don't consider present element in seq
+        //         len = max(len, maxLen(nums, curIdx+1, prevIdx, dp));
+        //     }
+        // }
+
+        if(prevIdx == -1 or nums[curIdx] > nums[prevIdx]){
             // consider present element in seq
             len = max(len, 1 + maxLen(nums, curIdx+1, curIdx, dp));
+        } 
 
-            // don't consider present element in seq
-            len = max(len, maxLen(nums, curIdx+1, -1, dp));
-        }
-        else{
-            if(nums[curIdx] > nums[prevIdx]){
-                // consider present element in seq
-                len = max(len, 1 + maxLen(nums, curIdx+1, curIdx, dp));
-
-                // don't consider present element in seq
-                len = max(len, maxLen(nums, curIdx+1, prevIdx, dp));
-            }
-            else {
-                // don't consider present element in seq
-                len = max(len, maxLen(nums, curIdx+1, prevIdx, dp));
-            }
-        }
+        // don't consider present element in seq
+        len = max(len, maxLen(nums, curIdx+1, prevIdx, dp));
 
         if(prevIdx != -1) return dp[curIdx][prevIdx] = len;
         return len;
