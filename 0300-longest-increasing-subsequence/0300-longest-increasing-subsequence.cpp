f3c48b1129f32cf7739d431a2012ffc89afd0dbe
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-02-24 13:59:23 +0530

    Time: 703 ms (5.08%), Space: 296.7 MB (6.62%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int sz = nums.size();
+        vector<vector<int>> dp(sz+1, vector<int>(sz+1, -1));
+        return maxLen(nums, 0, -1, dp);
+    }
+
+    int maxLen(vector<int>& nums, int curIdx, int prevIdx, vector<vector<int>> &dp){
+        if(curIdx == nums.size()) return 0;
+
+        int len = INT_MIN;
+
+        if(prevIdx != -1 and dp[curIdx][prevIdx] != -1) return dp[curIdx][prevIdx];
+
+        if(prevIdx == -1){
+            // consider present element in seq
+            len = max(len, 1 + maxLen(nums, curIdx+1, curIdx, dp));
+
+            // don't consider present element in seq
+            len = max(len, maxLen(nums, curIdx+1, -1, dp));
+        }
+        else{
+            if(nums[curIdx] > nums[prevIdx]){
+                // consider present element in seq
+                len = max(len, 1 + maxLen(nums, curIdx+1, curIdx, dp));
+
+                // don't consider present element in seq
+                len = max(len, maxLen(nums, curIdx+1, prevIdx, dp));
+            }
+            else {
+                // don't consider present element in seq
+                len = max(len, maxLen(nums, curIdx+1, prevIdx, dp));
+            }
+        }
+
+        if(prevIdx != -1) return dp[curIdx][prevIdx] = len;
+        return len;
+    }
+};
