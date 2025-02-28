db7d775226ee70299304a9003ed60ffad4d06100
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-02-28 12:08:41 +0530

    Time: 29 ms (6.72%), Space: 21.3 MB (80.24%) - LeetHub

--- 2687-lexicographically-smallest-beautiful-string/2687-lexicographically-smallest-beautiful-string.cpp
+++ 2687-lexicographically-smallest-beautiful-string/2687-lexicographically-smallest-beautiful-string.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     bool isPalin(string &s, char curChar, int idx){
         if(idx-1 >= 0 and s[idx-1] == curChar) return true;
-        // if(idx+1 < s.size() and s[idx+1] == curChar) return true;
-        // if(idx-1 >= 0 and idx+1 < s.size() and s[idx-1] == s[idx+1]) return true;
         if(idx-2 >= 0 and s[idx-2] == curChar) return true;
         return false;
     }
@@ -11,14 +9,9 @@ public:
     void fixSuffix(string &s, int idx){
         for(int i=idx; i<s.size(); i++){
             for(char chr='a'; chr<='z'; chr++){
-                // cout<<"idx: "<<i<<" curChar "<< chr <<" "<<s<<endl; 
-                if((i-1 >= 0 and chr == s[i-1]) or (i-2 >= 0 and s[i-2] == chr)) continue;
-                
-                // cout<<"idx2: "<<i<<" curChar "<< chr<<" "<<s <<endl; 
-                
+                if(isPalin(s, chr, i)) continue;
                 s[i] = chr;
                 break;
-                
             }
         }
     }
@@ -31,11 +24,8 @@ public:
                 char curChar = (char)('a' + i);
                 if(curChar <= s[end]) continue;
 
-                // cout<<curChar <<" " <<s[end]<<" "<<end<<endl;
                 if(!isPalin(s, curChar, end)){
-                    // cout<<"str1 "<<s<<endl;
                     s[end] = curChar;
-                    // cout<<"str2 "<<s<<endl;
                     fixSuffix(s, end+1);
                     return s;
                 }
