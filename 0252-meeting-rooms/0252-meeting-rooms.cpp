a159b057d24353489e213200bc5daccc88569e8b
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-03-02 17:11:36 +0530

    Time: 0 ms (100%), Space: 16 MB (49.92%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.cpp
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
 
-    static bool compare(vector<int> int1, vector<int> int2){
-        return int1[0] <= int2[0];
-    }
-
     bool canAttendMeetings(vector<vector<int>>& intervals) {
         int sz = intervals.size();
-        sort(intervals.begin(), intervals.end(), compare);
+        sort(intervals.begin(), intervals.end());
 
         for(int i=0; i<sz-1; i++){
             if(intervals[i][1] > intervals[i+1][0]) return false;
