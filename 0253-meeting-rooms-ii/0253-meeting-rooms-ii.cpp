0bb7ce21cab497a5db4eb50f9089bb18b404b903
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-03-02 17:29:58 +0530

    Time: 1 ms (71.51%), Space: 16.1 MB (85.23%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
 
-    struct compare{
-        bool operator()(vector<int> int1, vector<int> int2){
-            return int1[1] > int2[1];
+    struct Compare {
+        bool operator()(pair<int, int> p1, pair<int, int> p2){
+            return p1.first > p2.first;
         }
-    }; 
+    };
 
     int minMeetingRooms(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
         int sz = intervals.size();
-        priority_queue<vector<int>, vector<vector<int>>, compare> minHeap; 
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> minHeap;
 
-        int minRooms = 1;
+        int ans = 1;
+        minHeap.push({intervals[0][1], intervals[0][0]});
 
-        for(int i=0; i < sz; i++){
-            if(minHeap.empty())
-            {
-                minHeap.push(intervals[i]);
-            } 
-            else if(!minHeap.empty())
-            {
-                int endTime = minHeap.top()[1];
-                if(endTime <= intervals[i][0])
-                {
-                    minHeap.pop();
-                    minHeap.push(intervals[i]);
-                } else 
-                {
-                    minHeap.push(intervals[i]);
-                    int sz1 = minHeap.size();
-                    minRooms = max(minRooms, sz1);
-                }
+        for(int i=1; i < sz; i++){
+            if(intervals[i][0] >= minHeap.top().first){
+                minHeap.pop();
+                minHeap.push({intervals[i][1], intervals[i][0]});
+            }
+            else {
+                ans++;
+                minHeap.push({intervals[i][1], intervals[i][0]});
             }
         }
 
-        return minRooms;
+        return ans;
     }
 };
