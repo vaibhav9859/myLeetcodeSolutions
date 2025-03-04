f2f8465deba413fb4fe72faec772553a1be25801
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-03-04 13:24:20 +0530

    Time: 0 ms (100%), Space: 19.8 MB (52.89%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int numCities = isConnected.size();
-        vector<int> vis(numCities+1, 0);
+        int n = isConnected.size();
+        vector<int> vis(n, 0);
         int prov = 0;
-        for(int i=0; i<numCities; i++){
-            if(vis[i] == 0){
-                vis[i] = 1;
+
+        for(int i=0; i<n; i++){
+            if(!vis[i]){
                 prov++;
-                dfs(isConnected, i, numCities, vis);
+                bfs(isConnected, i, vis, n);
             }
         }
 
         return prov;
     }
 
-    void dfs(vector<vector<int>>& isConn, int src, int numCities, vector<int> &vis){
+    void bfs(vector<vector<int>>& isConn, int curNode, vector<int> &vis, int n){
+        queue<int> q;
+        q.push(curNode);
+        vis[curNode] = 1;
 
-        for(int i=0; i<numCities; i++){
-            if(isConn[src][i] == 1 and src != i and vis[i] != 1){
-                vis[i] = 1;
-                dfs(isConn, i, numCities, vis);
+        while(!q.empty()){
+            int sz = q.size();
+  
+            while(sz--){
+                int curNode = q.front();
+                q.pop();
+                for(int i=0; i<n; i++){
+                    if(isConn[curNode][i] == 1 and curNode != i and !vis[i]){
+                        vis[i] = 1;
+                        q.push(i);
+                    }
+                }
             }
+            
         }
+
+        return;
     }
 };
