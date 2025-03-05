c577c4fdd302c0926bda5a43e580d973003c5a92
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-03-05 15:52:18 +0530

    Time: 19 ms (57.2%), Space: 35.6 MB (66.48%) - LeetHub

--- 0694-number-of-distinct-islands/0694-number-of-distinct-islands.cpp
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
-    int numDistinctIslands(vector<vector<int>>& grid) {
-        unordered_set<string> islands;
-        int rows = grid.size();
-        int cols = grid[0].size();
+    int rows;
+    int cols;
+    set<vector<pair<int, int>>> islandsSet;
+    vector<vector<int>> vis;
+    vector<vector<int>> dir = {{0,1}, {1,0}, {0,-1}, {-1,0}};
 
-        vector<vector<int>> vis(rows, vector<int>(cols, 0));
-        vector<vector<int>> dir = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-        string dirStr = "RDLU";
-        
+    int numDistinctIslands(vector<vector<int>>& grid) {
+        rows = grid.size();
+        cols = grid[0].size();
+        vis.assign(rows, vector<int>(cols, 0));
+
         for(int row=0; row<rows; row++){
             for(int col=0; col<cols; col++){
-                if(grid[row][col] == 1 and !vis[row][col]){
+                if(vis[row][col] == 0 and grid[row][col] == 1){
                     vis[row][col] = 1;
-                    string curDir;
-                    dfs(grid, row, col, vis, islands, dir, dirStr, curDir);
-                    islands.insert(curDir);
+                    vector<pair<int,int>> curTrav;
+                    curTrav.push_back({0, 0});
+                    dfs(grid, row, col, row, col, curTrav);
+                    islandsSet.insert(curTrav);
                 }
             }
         }
 
-        return islands.size();
+        return islandsSet.size();
     }
 
-    void dfs(vector<vector<int>>& grid, int row, int col, vector<vector<int>>& vis, unordered_set<string>& islands, vector<vector<int>> &dir, string &dirStr, string &curDir){
-        int rows = grid.size();
-        int cols = grid[0].size();
+    void dfs(vector<vector<int>>& grid, int baseRow, int baseCol, int row, int col, vector<pair<int,int>> &curTrav){
 
         for(int i=0; i<4; i++){
             int newRow = row + dir[i][0];
             int newCol = col + dir[i][1];
 
-            if(newRow >= 0 and newCol >=0 and newRow < rows and newCol < cols and grid[newRow][newCol] == 1 and vis[newRow][newCol] == 0){
+            if(newRow >= 0 and newCol >= 0 and newRow < rows and newCol < cols and !vis[newRow][newCol] and grid[newRow][newCol]){
                 vis[newRow][newCol] = 1;
-                curDir += dirStr[i];
-                dfs(grid, newRow, newCol, vis, islands, dir, dirStr, curDir);
-                
+                curTrav.push_back({newRow-baseRow, newCol-baseCol});
+                dfs(grid, baseRow, baseCol, newRow, newCol, curTrav);
             }
         }
-
-        curDir += 'B';
     }
 };
