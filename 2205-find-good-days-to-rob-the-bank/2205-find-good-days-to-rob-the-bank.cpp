bba53474357c6ce58d46b2843e455020c700c191
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-03-06 15:55:33 +0530

    Time: 22 ms (10.78%), Space: 94.4 MB (15.69%) - LeetHub

---
+++ 2205-find-good-days-to-rob-the-bank/2205-find-good-days-to-rob-the-bank.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> goodDaysToRobBank(vector<int>& security, int time) {
+        int sz = security.size();
+        vector<int> trackIncreasing(sz, 0);
+        vector<int> trackDecreasing(sz, 0);
+
+        vector<int> goodDaysIdx;
+
+        for(int i=0; i<sz; i++){
+            if(i==0){
+                trackIncreasing[i] = 0;
+                trackDecreasing[sz-i-1] = 0;
+            }
+            else {
+                if(security[i] <= security[i-1]){
+                    trackIncreasing[i] = trackIncreasing[i-1] + 1;
+                }
+                else {
+                    trackIncreasing[i] = 0;
+                }
+                
+                if(security[sz-i-1] <= security[sz-i]) {
+                    trackDecreasing[sz-i-1] = trackDecreasing[sz-i] + 1;
+                }
+                else {
+                    trackDecreasing[sz-i-1] = 0;
+                }
+            }
+        }
+
+        for(int i=0; i<sz; i++){
+            if(trackIncreasing[i] >= time and trackDecreasing[i] >= time){
+                goodDaysIdx.push_back(i);
+            }
+        }
+
+        return goodDaysIdx;
+    }
+};
