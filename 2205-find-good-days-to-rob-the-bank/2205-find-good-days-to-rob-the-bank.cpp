bb31c11a86e67785a71d1564734fbcf573a9a1de
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-03-06 16:03:25 +0530

    Time: 12 ms (32.04%), Space: 94.2 MB (74.35%) - LeetHub

--- 2205-find-good-days-to-rob-the-bank/2205-find-good-days-to-rob-the-bank.cpp
+++ 2205-find-good-days-to-rob-the-bank/2205-find-good-days-to-rob-the-bank.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     vector<int> goodDaysToRobBank(vector<int>& security, int time) {
         int sz = security.size();
+
+        // below vectors to track for how long array was increasing or decreasing
         vector<int> trackIncreasing(sz, 0);
         vector<int> trackDecreasing(sz, 0);
 
         vector<int> goodDaysIdx;
 
+        // below we can write 2 different for loops as well
         for(int i=0; i<sz; i++){
             if(i==0){
                 trackIncreasing[i] = 0;
@@ -29,6 +32,7 @@ public:
             }
         }
 
+        // using those arrays
         for(int i=0; i<sz; i++){
             if(trackIncreasing[i] >= time and trackDecreasing[i] >= time){
                 goodDaysIdx.push_back(i);
