7b50f152c896505127893ef153ad3cda42588217
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-03-08 11:55:07 +0530

    Time: 59 ms (84.78%), Space: 74.8 MB (87.99%) - LeetHub

---
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        // sort events
+        int sz = events.size();
+        int count = 0;
+        sort(events.begin(), events.end());
+
+        // create min heap and put only end day
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        // get the max days possible
+        int maxDays = 0;
+        for(auto &val: events){
+            maxDays = max(maxDays, val[1]);
+        }
+
+        // run a loop starting from day 1 till max days
+        // make sure to push events which start on this current day
+        // make sure to get out the events jiske end day < curday
+
+        // MAIN LOGIC: Now choose top from heap for the current day
+        // pop that from heap... keep running the loop
+        int curEvent = 0;
+
+        for(int day=1; day<=maxDays; day++){
+            while(!minHeap.empty() and minHeap.top() < day){
+                minHeap.pop();
+            }
+            
+            while(curEvent<sz and events[curEvent][0] <= day){
+                minHeap.push(events[curEvent][1]);
+                curEvent++;
+            }
+
+            if(!minHeap.empty()){
+                count++;
+                minHeap.pop();
+            }
+
+        }
+
+        return count;
+    }
+};
