082afd1977e1efb38d3fe363c063ebc78f0e5bd1
Vaibhav Deshmukh <36755262+vaibhav9859@users.noreply.github.com>
2025-03-13 14:20:52 +0530

    Time: 0 ms (100%), Space: 59.3 MB (25.99%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -4,15 +4,22 @@ public:
         int l = 0, r = 0, zeroCount = 0, maxLen = 0;
 
         while (r < nums.size()) {
+            // checking value at r
             if (nums[r] == 0) zeroCount++;
 
+            // update length only when zeroCount is valid and l <= r
+            if(zeroCount <= k and l <= r) maxLen = max(maxLen, r - l + 1);
+
+            // gonna move l only when zeroCount is more than "k"
             if (zeroCount > k) {
                 if (nums[l] == 0) zeroCount--;
-                l++;  // Shrink window
+                l++;  
             }
 
-            if(zeroCount <= k and l <= r) maxLen = max(maxLen, r - l + 1);
-            r++;  // Expand window
+            
+
+            // at every condition keep moving r
+            r++;  
         }
 
         return maxLen;
